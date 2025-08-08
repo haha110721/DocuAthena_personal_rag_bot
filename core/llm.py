6f07d13183ea2868c29e17e81bf4b8e6c7078ac9
haha110721 <haha110721@gmail.com>
2025-08-08 17:12:31 +0800

    Add call oss function

--- core/llm.py
+++ core/llm.py
@@ -2,17 +2,13 @@ from transformers import pipeline
 from core.embed.embedder import Embedder
 from core.vectorstore.vectorstore import VectorStore
 from config import VECTORSTORE_PATH
+from ollama import chat
 
 
 class RAGQA:
     def __init__(self):
         self.embedder = Embedder()
         self.vdb = VectorStore(VECTORSTORE_PATH, self.embedder)
-        self.llm = pipeline(
-            "text-generation",
-            model="Qwen/Qwen2-0.5B-Instruct",
-            device_map="cpu"
-        )
 
     def ask(self, question):
         docs = self.vdb.similarity_search(query=question)
@@ -31,9 +27,52 @@ class RAGQA:
             請根據上述內容，給出完整且有條理的答案，如果文件中沒有提到，就說「文件中沒有提到」。
         """
 
-        result = self.llm(prompt, max_length=1024, do_sample=True)
+        answer = self._call_ollama(prompt)
+        return answer
+
+    def _call_qwen(self, prompt):
+        llm = pipeline(
+            "text-generation",
+            model="Qwen/Qwen2-0.5B-Instruct",
+            device_map="cpu"
+        )
+
+        result = llm(prompt, max_length=1024, do_sample=True)
         output = result[0]["generated_text"]
 
         # 去掉 prompt 部分，只保留生成內容
         answer = output[len(prompt):].strip()
         return answer
+
+    def _call_ollama(self, prompt):
+        # url = "http://127.0.0.1:11434/v1/chat/completions"  # 本地的 Ollama 服務
+        # headers = {
+        #     "Content-Type": "application/json",
+        # }
+        # payload = {
+        #     "model": "gpt-oss:20b",
+        #     "messages": [
+        #         {"role": "user", "content": prompt}
+        #     ]
+        # }
+
+        # response = requests.post(url, json=payload, headers=headers)
+        # response.raise_for_status()
+        # data = response.json()
+    
+        full_response = ""
+
+        response = chat(
+            model="gpt-oss:20b",
+            messages=[
+                {"role": "user", "content": prompt}
+            ],
+            stream=True
+        )
+
+        for chunk in response:
+            print(chunk['message']['content'], end='', flush=True)
+            full_response += chunk['message']['content']
+
+        print()
+        return full_response
